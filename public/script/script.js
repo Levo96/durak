a55c54f69fdad3db214b94907f539ea7bfdd611c
Levan96 <levan.mebonia.it@gmail.com>
2021-02-15 17:27:06 +0100

    durak not finished

---
+++ public/script/script.js
@@ -0,0 +1,132 @@
+/* ------------------ GAME --------------------- */
+
+
+/* ----------- DOM  ELEMENTS ------------*/
+
+/* --------------- HOMEPAGE -------------------------------- */
+
+/*--------Containers ------------*/
+let gameOptionContainer = document.getElementById('OptionButtonContainer');
+let createRoomContainer = document.getElementById('createRoomContainer');
+let joinRoomContainer = document.getElementById('joinRoomContainer');
+let roomContainer = document.getElementById('roomContainer');
+/* ---------------------- Buttons --------------- */
+let toCreateRoomBtn = document.getElementById('toCreateRoomBtn');
+let toJoinRoomBtn = document.getElementById('toJoinRoomBtn');
+let vsCpuBtn = document.getElementById('toVsCpuBtn');
+let createRoomBtn = document.getElementById('createRoomBtn');
+let joinRoomBtn = document.getElementById('joinRoomBtn');
+let createRoomBackBtn = document.getElementById('createRoomBackBtn');
+let joinRoomBackBtn = document.getElementById('joinRoomBackBtn');
+/* -------------------INPUTS --------------------------*/
+let createRoomInput = document.getElementById('createRoomInput');
+let joinRoomInput = document.getElementById('joinRoomInput');
+
+/* ----------HOMEPAGE BASIC NAVIGATION FUNCTIONS ------------- */
+let hideCreateAndJoinRoomContainer = () =>
+{
+  $(createRoomContainer).hide();
+  $(joinRoomContainer).hide();
+  $(joinRoomInput).val("");
+  $(createRoomInput).val("");
+}
+
+let hideCreateRoomContainer = () =>
+{
+  $(createRoomInput).val("");
+  $(createRoomContainer).hide();
+  $(gameOptionContainer).fadeIn();
+}
+
+let hideJoinRoomContainer = () =>
+{
+  $(joinRoomInput).val("");
+  $(joinRoomContainer).hide();
+  $(gameOptionContainer).fadeIn();
+}
+
+let showCreateRoomContainer = () =>
+{
+  $(gameOptionContainer).hide();
+  $(createRoomContainer).fadeIn();
+}
+
+let showJoinRoomContainer = () =>
+{
+  $(gameOptionContainer).hide();
+  $(joinRoomContainer).fadeIn();
+}
+
+$(toCreateRoomBtn).on('click', ()=> {
+  showCreateRoomContainer();
+});
+
+$(toJoinRoomBtn).on('click', ()=> {
+  showJoinRoomContainer();
+});
+
+$(createRoomBackBtn).on('click',()=> {
+  hideCreateRoomContainer();
+});
+
+$(joinRoomBackBtn).on('click',()=> {
+  hideJoinRoomContainer();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ----------------GAME START-------------------- */
+hideCreateAndJoinRoomContainer();
---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.set('views', path.join(__dirname,'views'));
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.route('/').get((req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+let onPort = process.env.PORT || 5000
+http.listen(onPort, ()=> {
+  console.log("Listening on Port: " + onPort);
+});
